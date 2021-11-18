d5325ee40fe70cffe3f0b19bd8546cbed33462e4
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2021-11-18 18:21:51 +0330

    added moving to head

--- Assets/Scripts/HeadController.cs
+++ Assets/Scripts/HeadController.cs
@@ -1,36 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Random = UnityEngine.Random;
 
 public class HeadController : MonoBehaviour
 {
-    public Directions direction ;//= Random.Range(0, (int) Directions.Count);
-    [Range(0f, 1f)] public float moveFrequency;
+    public Directions direction ;
+    
+    
+    [Range(0f, 1f)] public float moveFreq;
+    public float counter;
+    
+    
     [Range(0f, 1f)] public float moveAmount;
+
+
+    private List<Vector3> _directions;
+
+
     [SerializeField] public GameObject bodyPrefab;
+
+
+    
+    
     
     void Start()
     {
         
+        counter = 0f;
+
+    }
+
+    private void Awake()
+    {
+        direction = (Directions)Random.Range(0, (int) Directions.Count);
+
+        _directions = new List<Vector3>()
+        {
+            new Vector3(0f,0f,moveAmount),
+            new Vector3(0f,0f,-moveAmount),
+            new Vector3(moveAmount,0f,0f),
+            new Vector3(-moveAmount,0f,0f),
+        };
     }
 
     void Update()
     {
-        if (Input.GetKey(KeyCode.D))
+        if (CanMove())
+            transform.Translate(_directions[(int)direction]);
+
+
+        if (Input.GetKey(KeyCode.D) && direction!=Directions.Left)
+            direction =(Directions.Right);
+        if (Input.GetKey(KeyCode.A) && direction!=Directions.Right)
+            direction = (Directions.Left);
+        if (Input.GetKey(KeyCode.W) && direction!=Directions.Down)
+            direction = (Directions.Up);
+        if (Input.GetKey(KeyCode.S) && direction!=Directions.Up)
+            direction = (Directions.Down);
+    }
+    
+
+    private bool CanMove()
+    {
+        counter += Time.deltaTime;
+        if (counter >= moveFreq)
         {
-            transform.position += new Vector3(moveAmount, 0, 0);
+            counter = 0f;
+            return true;
         }
-        if (Input.GetKey(KeyCode.A))
-        {
-            transform.position += new Vector3(-moveAmount, 0, 0);
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            transform.position += new Vector3(0, 0, moveAmount);
-        }
-        if (Input.GetKey(KeyCode.S))
-        {
-            transform.position += new Vector3(0, 0, -moveAmount);
-        }
+        return false;
     }
 }

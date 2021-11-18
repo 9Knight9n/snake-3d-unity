97266d0f40d7427f117d6df895cc3f509102b615
Mohamad Sajad Naghizadeh <naghizadeh.mdsd@gmail.com>
2021-11-18 14:44:38 +0330

    changed light and camera rotation

---
+++ Assets/Scripts/HeadController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HeadController : MonoBehaviour
+{
+    [Range(0f, 1f)] public float moveAmount;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D))
+        {
+            transform.position += new Vector3(moveAmount, 0, 0);
+        }
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.position += new Vector3(-moveAmount, 0, 0);
+        }
+    }
+}
